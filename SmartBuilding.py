5810a8f1cb48a6a30d0fb253025e805b4e4a3602
taegi <ffff4565@naver.com>
2022-11-24 11:23:47 +0900

    주석달기 & 불필요한 코드 삭제

--- SmartBuilding.py
+++ SmartBuilding.py
@@ -23,7 +23,7 @@ conn=None
 cur=None
 
 sql=""
-
+#mysql 연결용 변수
 conn=pymysql.connect(host=config['hosturl'],user=config['userID'],password=config['password'],db='smartbuilding',charset='utf8')
 cur=conn.cursor()
 
@@ -32,7 +32,7 @@ cur=conn.cursor()
 #---------------------------------------------------------------open csv 파일 데이터 가져오기--------------------------------------------------------------------
 
 
-# get all files inside a specific folder
+# RoomList에 smartbuildingsystem 데이터를 넣을 예정 . room 파일을 넣으면 그 파일을 읽음.
 dir_path = '/Users/user/Desktop/project/SmartBuildingDB/RoomList/*'
 recentfolders=[]
 roomlist=[]
@@ -41,58 +41,30 @@ roomdatas=[]
 
 
 countnow=0
+#scheduler에 사용할 함수. 매 1초마다 작동(이미 정해져있는 데이터이고,시간값도 파일에 있는 데이터를 가져오기 때문에 시간을 짧게 함, 만약 실제 센서를 이용한다면 5초~10초 사용하기)
 def job():
+    #smartbuilding데이터중 pir센서만 10초마다 나머지는 5초마다 작동해서 우선 임시로 2번중 1번만 pir센서를 읽기 위해서 count변수를 사용함
     global countnow
     folders = glob.glob(dir_path)
     for folder in folders:
+        # 새로운 폴더인지 아닌지 확인하여 새로운 폴더일 경우 안에 있는 파일을 읽어옴,안에 있는 새로운 csv 파일들까지 다 확인하기에는 조금 어려울것같다.
         if folder not in recentfolders:
             print("새로운 방 : ",folder)
             recentfolders.append(folder)
             csvfiles=[file for file in os.listdir(folder) if file.endswith('csv')]
             df= None
-            table=[] 
-            timestamp=None           
-            co2=None
-            humidity=None
-            light=None
-            pir=None
-            temperature=None
             roomname=folder.replace("/Users/user/Desktop/project/SmartBuildingDB/RoomList\\","")
             roomlist.append(roomname)
-            name=roomname
-            def setname(i):
-                return roomname
-            def getdf(item):
-                if item=="co2.csv":
-                    return (pd.read_csv(folder+"/"+item,header=None,names=["ctime","co2"]))
-                    # timestamp=temp[0].values
-                    # co2=temp[1].values
-                elif item=="humidity.csv":    
-                    return pd.read_csv(folder+"/"+item,header=None,names=["htime","humidity"])
-                    # humidity=temp[1].values
-                elif "light.csv"==item:
-                    return pd.read_csv(folder+"/"+item,header=None,names=["ltime","light"])
-                    # light=temp[1].values
-                elif "pir.csv"==item:
-                    # pir=temp[1].values
-                    return pd.read_csv(folder+"/"+item,header=None,names=["ptime","pir"])    
-                else :
-                    # temperature=temp[1].values     
-                    return pd.read_csv(folder+"/"+item,header=None,names=["ttime","temperature"])
-
-            # ss=pd.concat(getdf(item) for item in csvfiles)
-            # ss.index=[roomname for i in range(len(ss.index))]
 
             for i in csvfiles:
-                # temp=pd.read_csv(folder+"/"+i,header=None)
 
                 if i=="co2.csv":
+                    #처음엔 dataframe타입이 아니라 None타입으로 들어있어 두 데이터프레임을 합치는 것이 아닌 대입 연산자를 적용 
                     if df is None:
                         df=pd.read_csv(folder+"/"+i,header=None,names=["ctime","co2"])
                         continue
                     df=pd.concat([df,pd.read_csv(folder+"/"+i,header=None,names=["ctime","co2"])],axis=1)
-                    # timestamp=temp[0].values
-                    # co2=temp[1].values
+
                 elif i=="humidity.csv":
                     if df is None:
                         df=pd.read_csv(folder+"/"+i,header=None,names=["htime","humidity"])
@@ -103,8 +75,6 @@ def job():
                         pdf=pd.read_csv(folder+"/"+i,header=None,names=["ltime","light"])
                         continue
                     df=pd.concat([df,pd.read_csv(folder+"/"+i,header=None,names=["ltime","light"])],axis=1)
-
-
                 elif "pir.csv"==i:
                     if df is None:
                         df=pd.read_csv(folder+"/"+i,header=None,names=["ptime","pir"])
@@ -115,8 +85,8 @@ def job():
                         df=pd.read_csv(folder+"/"+i,header=None,names=["ttime","temperature"])
                         continue
                     df=pd.concat([df,pd.read_csv(folder+"/"+i,header=None,names=["ttime","temperature"])],axis=1)
-                table.append(i.replace(".csv",""))
             df = df.astype(object).where(pd.notnull(df), None)
+            #방 이름을 데이터프레임 인덱스로 지정
             df.index=[roomname for i in range(len(df.index))]
 
             roomdatas.append(df)
@@ -129,12 +99,12 @@ def job():
         else:
             return (str(value.name),int(value['ctime']),int(value['co2']),int(value['htime']),float(value['humidity']),int(value['ltime']),int(value['light']),int(value['ttime']),float(value['temperature']))
 
-    #우선은 pir 값이 다른 센서에 비해 2배 적기 때문에 
+    #우선은 pir 값이 다른 센서에 비해 2배 적기 때문에 현재 count값에 따라 sql을 달리함
     if countnow%2==0:
         sql='INSERT INTO roomdata (Room,ctime,co2,htime,humidity,ltime,light,ptime,pir,ttime,temperature) values ("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")'
     else:
         sql='INSERT INTO roomdata (Room,ctime,co2,htime,humidity,ltime,light,ttime,temperature) values ("%s","%s","%s","%s","%s","%s","%s","%s","%s")'
-    #
+    # DB에 sql문을 내보내는 함수, excutemany로 하여 모든 방의 countnow번째 데이터를 INSERT한다.
     cur.executemany(sql,[loaddata(t) for t in roomdatas])
     print(cur.rowcount,"만큼 입력됨")
     conn.commit()	# 저장
